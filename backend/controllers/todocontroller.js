192545aaf36fb136ed7409d5b639bd47eb8303a9
Hammemi Mariem <hammemimariem3@gmail.com>
2025-10-31 11:36:19 +0100

    deleteandupdatemethodes

--- backend/controllers/todocontroller.js
+++ backend/controllers/todocontroller.js
@@ -19,4 +19,39 @@ const gettodo = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-module.exports = { addtodo,gettodo };
+//update user
+const updatetodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findOne({ _id: id, user: req.user._id });
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found or not authorized" });
+    }
+    // Update fields
+     todo.title = req.body.title || todo.title;
+    todo.description = req.body.description || todo.description;
+    todo.completed = req.body.completed ?? todo.completed;
+
+    const updatedTodo = await todo.save();
+    res.status(200).json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+//delete user
+const deletetodo = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await Todo.findOneAndDelete({ _id: id, user: req.user._id });
+
+    if (!todo) {
+      return res.status(404).json({ message: "Todo not found or not authorized" });
+    }
+
+    res.status(200).json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+module.exports = { addtodo, gettodo, updatetodo, deletetodo };
--- backend/routes/todoRoutes.js
+++ backend/routes/todoRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const Todo = require('../models/todo');
-const { addtodo, gettodo } = require('../controllers/todocontroller');
+const { addtodo, gettodo,updatetodo,
+  deletetodo } = require('../controllers/todocontroller');
 const router = express.Router();
 // Create a new todo (only if logged in)
 router.post('/', protect,addtodo );
 
 // Get todos for logged-in user
 router.get('/', protect,gettodo );
-
+// Update a todo by ID (only if logged in)
+router.put('/:id', protect,updatetodo );
+// Delete a todo by ID (only if logged in)
+router.delete('/:id', protect,deletetodo );
 module.exports = router;
